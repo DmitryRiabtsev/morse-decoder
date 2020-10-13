d170cd0438b679539bb29e53c0fb4cbfef095ac5
Dmitry <dim4ik7558118@gmail.com>
2020-10-14 00:07:36 +0300

    test:test done

--- src/index.js
+++ src/index.js
@@ -36,11 +36,38 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
 function decode(expr) {
-    // write your solution here
+    let arr = expr.match(/.{1,10}/g);
+    let array =[];
+    for(var i=0; i<arr.length; i++){
+    array.push(arr[i].match(/.{1,2}/g));
 }
-
+for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array[i].length; j++) {
+        if(array[i][j]=='00'){
+        delete array[i][j];
+        }
+        if(array[i][j]=='10'){
+        array[i][j]='.';
+        }
+        if(array[i][j]=='11'){
+        array[i][j]='-';
+        }
+    }
+    array[i] = array[i].join('');
+    if(array[i]=='**********'){
+    array[i]=' ';
+    }
+}
+for(let i=0; i<array.length; i++){
+    for(var key in  MORSE_TABLE){
+        if(key==array[i]){
+        array[i]= MORSE_TABLE[key];
+        }
+    }   
+}
+return array.join('');
+}
 module.exports = {
     decode
 }
